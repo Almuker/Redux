b94b76cc6852e7456fe84499ff7c5b1ad0cf6165
Almuker <73536819+Almuker@users.noreply.github.com>
2021-03-01 16:46:09 +0600

    added redux logger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './styles.css'
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import { rootReducer } from './redux/rootReducer'
 import { asyncIncrement, decrement, increment } from './redux/action'
 
@@ -10,7 +11,7 @@ const subBtn = document.getElementById('sub')
 const asyncBtn = document.getElementById('async')
 const themeBtn = document.getElementById('theme')
 
-const store = createStore(rootReducer, 0, applyMiddleware(thunk))
+const store = createStore(rootReducer, 0, applyMiddleware(thunk, logger))
 
 addBtn.addEventListener('click', ()=> {
     store.dispatch(increment())
